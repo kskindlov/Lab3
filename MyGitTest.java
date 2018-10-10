e85fa803e18341e352a7451448dc14f19a5d3058
Konar Skindlov <kskindlov@carroll.edu>
2018-10-10 14:27:38 -0600

    changed message in branch A

--- MyGitTest.java
+++ MyGitTest.java
@@ -3,6 +3,6 @@ public class MyGitTest
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Konar says Hello Git Test Branch AAAA.");
+		System.out.println("Konar says Hello Git Test Branch AAAAA.");
 	}
 }
